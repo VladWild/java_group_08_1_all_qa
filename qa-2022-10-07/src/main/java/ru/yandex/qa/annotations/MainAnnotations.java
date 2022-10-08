6d50b693f615ddc9dbc22bf532e94a6d6c53c737
VladWild <33370139+VladWild@users.noreply.github.com>
2022-10-08 12:01:38 +0300

    qa-2022-10-08 аннотации

--- qa-2022-10-07/src/main/java/ru/yandex/qa/annotations/MainAnnotations.java
+++ qa-2022-10-07/src/main/java/ru/yandex/qa/annotations/MainAnnotations.java
@@ -1,8 +1,58 @@
 package ru.yandex.qa.annotations;
 
+import java.lang.annotation.*;
+
 public class MainAnnotations {
 
+    //@SuppressWarnings("all")
     public static void main(String[] args) {
+        /*MyClass myClass = new MyClass();
+        myClass.method();
 
+        Thread thread = new Thread();
+        thread.stop();
+
+        Object o = null;
+        o.toString();*/
+
+        Class2 class2 = new Class2();
+        Class<? extends Class2> aClass = class2.getClass();
+
+        Annotation[] annotations = aClass.getAnnotations();
+        MyAnnotation annotation = (MyAnnotation) annotations[0];
+        String message = annotation.message();
+        int value = annotation.value();
+        System.out.println(message);
+        System.out.println(value);
+
+        MyAnnotation annotation2 = (MyAnnotation) aClass.getMethods()[0].getAnnotations()[0];
+        System.out.println(annotation2.message());
+        System.out.println(annotation2.value());
+    }
+
+    public static class Class1 {
+
+        public void method() {
+
+        }
+    }
+
+    @MyAnnotation(message = "я стою над классом")
+    public static class Class2 extends Class1 {
+
+        @Override
+        @MyAnnotation(message = "я стою над методом", value = 200)
+        public void method() {
+            //super.method();
+        }
+    }
+
+    @Documented
+    @Target({ElementType.TYPE, ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface MyAnnotation {
+        String message();
+
+        int value() default 100;
     }
 }
---
+++ qa-2022-10-07/src/main/java/ru/yandex/qa/annotations/MyClass.java
@@ -0,0 +1,9 @@
+package ru.yandex.qa.annotations;
+
+public class MyClass {
+
+    @Deprecated
+    public void method() {
+
+    }
+}
