1a0abb20f1644cfb0e1bb56c39eda6cf21f8a28f
VladWild <33370139+VladWild@users.noreply.github.com>
2022-11-05 12:11:13 +0300

    qa 2022-11-05 all

--- qa-2022-11-05/src/main/java/ru/yandex/qa/db/model/Book.java
+++ qa-2022-11-05/src/main/java/ru/yandex/qa/db/model/Book.java
@@ -3,12 +3,17 @@ package ru.yandex.qa.db.model;
 import ru.yandex.qa.db.model.base.BaseModel;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "books")
 public class Book extends BaseModel<Long> {
 
     private String title;
+
+    private Set<Page> pages = new HashSet<>();      //рекомендация ставить Set
+    private Set<Author> authors = new HashSet<>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,5 +27,39 @@ public class Book extends BaseModel<Long> {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    //если однонаправленная связь
+    //@OneToMany
+    //@JoinColumn(name = "book_id")
+    @OneToMany(mappedBy = "book")
+    public Set<Page> getPages() {
+        return pages;
+    }
+
+    public void setPages(Set<Page> pages) {
+        this.pages = pages;
+    }
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_author",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", id=" + id +
+                '}';
     }
 }
--- qa-2022-11-05/src/test/resources/sql/book/add_book.sql
+++ qa-2022-11-05/src/test/resources/sql/book/add_book.sql
@@ -1,5 +1,5 @@
 insert into books (id, title) values (1, 'Гарри Поттер');
-insert into books (id, title) values (2, 'Сталкер');
+insert into books (id, title) values (2, 'Гарри Поттер и Кубок Огня');
 insert into books (id, title) values (3, 'Метро 2033');
-insert into books (id, title) values (4, 'Бегущий в Либиринте');
-insert into books (id, title) values (5, 'Голодные игры');
+insert into books (id, title) values (4, 'Метро 2034');
+insert into books (id, title) values (5, 'Метро 2035');
