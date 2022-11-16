312216d190e330b4d2c900d1818e5d040fed7c18
VladWild <33370139+VladWild@users.noreply.github.com>
2022-11-16 18:38:54 +0300

    email

--- qa-2022-11-19/src/main/java/ru/yandex/qa/db/web/dto/request/BookRequestDto.java
+++ qa-2022-11-19/src/main/java/ru/yandex/qa/db/web/dto/request/BookRequestDto.java
@@ -1,10 +1,13 @@
 package ru.yandex.qa.db.web.dto.request;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 public class BookRequestDto {
     @NotBlank(message = "имя книги не должно быть пустым")
     private String title;
+    @Email
+    private String email;
 
     public String getTitle() {
         return title;
@@ -12,5 +15,13 @@ public class BookRequestDto {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
